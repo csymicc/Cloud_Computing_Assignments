08068242a89edcb9a4d24e3e6c36c8f39eb6eae4
csymicc <chenmickey@live.com>
2017-10-15 23:36:35 -0400

    Assignment2-2

--- Assignment2/source code/graphX/Graph.scala
+++ Assignment2/source code/graphX/Graph.scala
@@ -3,7 +3,7 @@ package pageRank
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.xml.{NodeSeq, XML}
-import org.apache.spark.graphx.VertexId
+import org.apache.spark.graphx.{GraphLoader, VertexId}
 
 
 object GraphPageRank {
@@ -46,6 +46,28 @@ object GraphPageRank {
     })
 
     relation.saveAsTextFile("/csy/edge")
+
+    val graph = GraphLoader.edgeListFile(sc, "/csy/edge")
+
+    val ranks = graph.pageRank(0.001).vertices
+
+    val titles = sc.textFile("/id/part-00000").map { line =>
+      val fields = line.replace("(", "").replace(")", "").split(",")
+      var name = fields(0)
+      if (fields.length > 2) {
+        for (i <- 1 to fields.length - 2)
+          name = name + ", " + fields(i)
+      }
+      (fields.last.toLong, name)
+    }
+
+    val result = titles.join(ranks).map {
+      case (id, (title, rank)) => (title, rank)
+    }.sortBy(_._2, false)
+
+    result.coalesce(1).saveAsTextFile("/csy/graphX")
+
+    sc.stop()
 
     sc.stop()
   }
